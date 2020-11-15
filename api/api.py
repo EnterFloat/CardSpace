c78b8f2a1b0b74df22fe655a7b67dd2c125cf4f8
Kasper Skov Hansen <kasperskov01@gmail.com>
2020-11-15 16:35:03 +0100

    Env vars and aws update

--- api/api.py
+++ api/api.py
@@ -22,21 +22,21 @@ import random
 from dotenv import load_dotenv
 load_dotenv()
 
-S3_BUCKET = os.environ['S3_BUCKET']
-S3_KEY = os.environ['S3_KEY']
-S3_SECRET = os.environ['S3_SECRET']
+S3_BUCKET_NAME = os.environ['S3_BUCKET_NAME']
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 
 
 app = Flask(__name__, static_folder='../build', static_url_path='/')
 cors = CORS(app)
 
-print(S3_BUCKET)
-print(S3_KEY)
-print(S3_SECRET)
+print(S3_BUCKET_NAME)
+print(AWS_ACCESS_KEY_ID)
+print(AWS_SECRET_ACCESS_KEY)
 
 # Connect to the S3 bucket and just drop it on there
 # s3 = boto3.client('s3', aws_access_key_id=S3_KEY, aws_secret_access_key=S3_SECRET)
-
+session = boto3.Session( aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
 
 @app.route('/api')
 @cross_origin()
@@ -47,9 +47,9 @@ def Welcome():
 @cross_origin()
 def GeneratePie():
     print("GeneratePie")    
-    print(S3_BUCKET)
-    print(S3_KEY)
-    print(S3_SECRET)
+    print(S3_BUCKET_NAME)
+    print(AWS_ACCESS_KEY_ID)
+    print(AWS_SECRET_ACCESS_KEY)
     # Get the input data (Wedge is the distance between slices) from the request    
     data = request.args.get('data')
     colors = request.args.get('colors')
@@ -70,9 +70,10 @@ def GeneratePie():
     # Save the image temporary on the local machine
     plt.savefig(os.getcwd() + '/chart.png')
 
-    s3_resource = boto3.resource("s3", aws_access_key_id=S3_KEY, aws_secret_access_key=S3_SECRET)
+    # s3_resource = boto3.resource("s3", aws_access_key_id=S3_KEY, aws_secret_access_key=S3_SECRET)
+    s3 = session.resource('s3')
     img_name = random.randint(1000, 10000)    
-    s3_resource.Bucket(S3_BUCKET).upload_file(os.getcwd() + '/chart.png', "charts/chart_" + str(img_name) + ".png", ExtraArgs={'ACL':'public-read'})
+    s3.Bucket(S3_BUCKET_NAME).upload_file(os.getcwd() + '/chart.png', "charts/chart_" + str(img_name) + ".png", ExtraArgs={'ACL':'public-read'})
 
     # s3_resource = boto3.resource("s3")
     # img_name = random.randint(1000, 10000)    
@@ -80,7 +81,7 @@ def GeneratePie():
 
     status = {}
     status['status'] = 'DONE'
-    status['message'] = 'https://'+ S3_BUCKET +'.s3.eu-north-1.amazonaws.com/charts/chart_' + str(img_name) + '.png'
+    status['message'] = 'https://'+ S3_BUCKET_NAME +'.s3.eu-north-1.amazonaws.com/charts/chart_' + str(img_name) + '.png'
     return make_response(jsonify(status), 200)
 
 @app.route('/')
